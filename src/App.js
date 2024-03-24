07a3a2cbea00afb4a50a036b293d6c6aa01570f7
yuchangseuk <ckdtjr428@naver.com>
2024-03-24 17:32:32 +0900

    apple coding 3강

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  let post = '강남 우동 맛집';
+  let post = '우남 우동 맛집';
+  let [글제목, b] = useState(['남자 코트 추천', '강남 맛집 추천', '파이썬 독학']);
   
-
+  
   return (
     <div className="App">
       <div className= "black-nav">
-        <h4 style={ {color : 'red', fontSize : '16px'} }>블로그임</h4> 
-         </div>
-      <h4>{post}</h4>
+        <h4>ReactBlog</h4> 
+      </div>
+      <div className='list'>
+        <h4>{글제목[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
