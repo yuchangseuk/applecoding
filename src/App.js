eb436542f295a94444051350acef473b5b29abe5
yuchangseuk <ckdtjr428@naver.com>
2024-03-24 18:59:13 +0900

    apple coding 4강

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable*/
+
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
@@ -6,17 +8,21 @@ function App() {
 
   let post = '우남 우동 맛집';
   let [글제목, b] = useState(['남자 코트 추천', '강남 맛집 추천', '파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
   
-  
+  //function 함수(){
+  //  console.log(1);
+  //}
   return (
     <div className="App">
       <div className= "black-nav">
         <h4>ReactBlog</h4> 
       </div>
       <div className='list'>
-        <h4>{글제목[0]}</h4>
+        <h4>{글제목[0]}<spen onClick={()=>{ 따봉변경(따봉+1)}}> 👍</spen> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
+
       <div className='list'>
         <h4>{글제목[1]}</h4>
         <p>2월 17일 발행</p>
